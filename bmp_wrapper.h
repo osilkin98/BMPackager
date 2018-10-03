fd1665f8db2d3f62c8f3bf541a61997c1d35eb32
Oleg Silkin <o.silkin98@gmail.com>
2018-10-03 09:21:49 -0500

    Adds the declarations for the BMAP header

--- bmp_wrapper.h
+++ bmp_wrapper.h
@@ -1,5 +1,30 @@
 #ifndef BINARY_BMP_WRAPPER_H
 #define BINARY_BMP_WRAPPER_H
 
+#include <stdint.h>
+
+
+/* struct to contain the headers for the bitmap */
+struct bmap_header {
+
+    /* this is the only variable which will actually differ */
+    uint32_t file_size;
+
+    
+    /* since signature is known, it can be set here  */
+    const uint8_t signature[] = {'B', 'M'};
+    /* the reservation will ALWAYS be 0 */
+    const uint32_t reserved = 0;
+    /* the offset for the data will ALWAYS be 54, 
+       which is really wasteful when you think about it */
+    const uint32_t data_offset = 54;
+};
+
+/* this is the meta-data header, which defines values necessary
+   to be rendered correctly as a BMP file. 
+ */
+struct bmap_info_header {
+    
+};
 
 #endif
