abd196752d8a2eb342bef81416456415a69062f4
Oleg Silkin <o.silkin98@gmail.com>
2018-10-03 11:22:13 -0500

    Moves implementation specific global defines to implementation file

--- bmp_wrapper.c
+++ bmp_wrapper.c
@@ -5,3 +5,16 @@
 
 #define BMAP_HEADER_SIZE 14           /* Length of the main header in bytes */
 #define BMAP_INFO_HEADER_SIZE 40      /* Length of the info header in bytes */
+
+
+
+/* all of these will be 0, however they can be changed at a later time
+   should whoever is modifying it find it valuable to do so. */
+#define COMPRESSION 0
+#define IMAGE_SIZE 0
+#define X_PIXELS_PER_M 0
+#define Y_PIXELS_PER_M 0
+#define COLORS_USED 0
+#define IMPORTANT_COLORS 0
+
+
--- bmp_wrapper.h
+++ bmp_wrapper.h
@@ -27,15 +27,6 @@ struct BMapHeader {
   const FOUR_BYTES data_offset;
 };
 
-/* all of these will be 0, however they can be changed at a later time
-   should whoever is modifying it find it valuable to do so. */
-#define COMPRESSION 0
-#define IMAGE_SIZE 0
-#define X_PIXELS_PER_M 0
-#define Y_PIXELS_PER_M 0
-#define COLORS_USED 0
-#define IMPORTANT_COLORS 0
-
 
 /* this is the meta-data header, which defines values necessary
    to be rendered correctly as a BMP file. 
@@ -72,12 +63,6 @@ struct BMapInfoHeader {
 };
 
 
-#undef COMPRESSION
-#undef IMAGE_SIZE
-#undef X_PIXELS_PER_M
-#undef Y_PIXELS_PER_M
-#undef COLORS_USED
-#undef IMPORTANT_COLORS
 
 
 
